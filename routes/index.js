a1d4bdbf7596edda4f0ca4c5a9056e543eae2e4a
aisprayogi <ais.prayogi@gmail.com>
2018-01-10 12:22:28 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Yogi' });
+  res.render('index', { title: 'Widita Farma' });
 });
 
 module.exports = router;
